8ba161ddbd62148c4765f870199f17b6bb1ff136
farjadfazli <farjadfazli@gmail.com>
2019-08-12 21:35:33 -0400

    added event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(item => 
+    item.addEventListener('mouseover', event => 
+    event.target.style.color = 'red'));
+
+navLinks.forEach(item => 
+    item.addEventListener('mouseout', event => 
+    event.target.style.color = 'blue'));
+
+let headings = document.querySelectorAll('h2');
+headings.forEach(item =>
+    item.addEventListener("click", event =>
+    event.target.style.fontSize = '5rem'));
+
+headings.forEach(item =>
+    item.addEventListener("dblclick", event =>
+    event.target.style.fontSize = '3.2rem'));
+
+let container = document.querySelector('body');
+window.addEventListener("scroll", event =>
+    container.style.backgroundColor = 'yellow');
+
+window.addEventListener("resize", event =>
+    headings.forEach(item =>
+        item.style.color = 'green'));
